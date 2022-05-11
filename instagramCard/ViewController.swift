3cd71dc74c5d073f264ae9a4b9cb699423e1dc1f
Shien <f0928652413@gmail.com>
2022-05-11 13:45:24 +0800

    Initial Commit

---
+++ instagramCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramCard
+//
+//  Created by 方仕賢 on 2022/5/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
